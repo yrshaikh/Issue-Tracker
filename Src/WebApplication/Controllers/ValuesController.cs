55dd3cd6c9051cfea232d9a0d204d15a04270b5b
Yasser Shaikh <yasser.shaikh@agoda.com>
2017-08-20 01:14:19 +0700

    First commit with base project.

---
+++ Src/WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
