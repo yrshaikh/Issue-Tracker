5ce4a0b2922ae0ada3aa4db7ece2b7c99dcf4e1c
Yasser Shaikh <yasser.shaikh@agoda.com>
2017-09-03 12:06:15 +0700

    Added a base theme for the website

---
+++ Src/WebApplication/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
