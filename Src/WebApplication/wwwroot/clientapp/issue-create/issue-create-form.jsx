d9395f8a6bd7c3239cf39cb1412b5e17e82f993b
Yasser Shaikh <yasser.shaikh@agoda.com>
2017-09-09 22:04:20 +0700

    Attempted to add form validations.

--- Src/WebApplication/wwwroot/clientapp/issue-create/issue-create-form.jsx
+++ Src/WebApplication/wwwroot/clientapp/issue-create/issue-create-form.jsx
@@ -3,22 +3,64 @@ import React, { Component } from 'react';
 class IssueCreateForm extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			title: ''
+			, description: ''
+			, validations: {
+				title: true
+			}
+		};
+	
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	  }
+	
+	handleChange(event) {
+		var name = event.target.name;
+		this.setState({[name]: event.target.value});
 	}
-	render () {
-		return (
-			<div id='issue-create-form' className='row custom-form'>
+	
+	fireValidations(identifier) {
+		var valid = false;
+		switch(identifier){
+			case 'title': {
+				valid = this.state.title > 4;
+			}
+			default: valid = true;
+		}
+		return valid;
+	}
 
+	handleSubmit(event) {
+		alert('boom');
+		this.state.validations.title = this.fireValidations('title');
+		event.preventDefault();
+	}
+	
+	render() {
+		return (
+			<form id='issue-create-form' className='row custom-form'>
 				<div className='col-md-9'>
 					<div className='form-group'>
 						<label>Title</label>
-						<input type='text' className='form-control text-box' tabIndex='1' 
-							placeholder='Enter a one-line summary of the issue.'>
+						<input type='text'
+							name='title'
+							className='form-control text-box' 
+							placeholder='Enter a one-line summary of the issue.'
+							value={this.state.title }
+							onChange={this.handleChange}>
 						</input>
+						{ !this.state.validations.title ? 'Title should atleast be of 10 characters' : '' }
 					</div>
 					<div className='form-group'>
 						<label>Description</label>
-						<textarea className='form-control text-area' rows='5'
+						<textarea 
+							className='form-control text-area'
+							name='description'
+							rows='5'
 							placeholder='Steps to reproduce, what you expected to see, and what you saw it instead.'
+							value={this.state.description }
+							onChange={this.handleChange}>
 						>
 						</textarea>
 					</div>
@@ -38,10 +80,10 @@ class IssueCreateForm extends Component {
 				</div>
 
 				<div className='col-md-12'>
-					<button id="create" type="submit" className="btn btn-success">Create Issue</button>
-					<button href="/projects" className="btn btn-outline-dark">Create and Add Another</button>
+					<button type='button' id='create' className='btn btn-success' onClick={this.handleSubmit}>Create Issue</button>
+					<button type='button' className='btn btn-outline-dark' onClick={this.handleSubmit}>Create and Add Another</button>
 				</div>
-			</div>
+			</form>
 		);
 	}
 }
