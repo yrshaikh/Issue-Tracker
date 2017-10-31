1f92a1d3276e22dbf2e3ab91b8c8a6dfdc0c117e
Yasser Shaikh <yrshaikh.mail@gmail.com>
2017-10-31 22:41:40 +0700

    added eslint.

--- Src/WebApplication/wwwroot/clientapp/root.jsx
+++ Src/WebApplication/wwwroot/clientapp/root.jsx
@@ -1,23 +1,23 @@
-﻿import IssueListApp from './issue-list-app';
-import IssueCreateEditApp from './issue-create-app';
-import IssueViewApp from './issue-view-app';
-import React from 'react';
+﻿import IssueListApp from './issue-list-app'; // eslint-disable-line no-unused-vars
+import IssueCreateEditApp from './issue-create-app'; // eslint-disable-line no-unused-vars
+import IssueViewApp from './issue-view-app'; // eslint-disable-line no-unused-vars
+
 import ReactDOM from 'react-dom';
 
 if (document.getElementById('app-issue-list-root'))
-	ReactDOM.render(
-		<IssueListApp />,
-		document.getElementById('app-issue-list-root')
-	);
+    ReactDOM.render(
+        <IssueListApp />,
+        document.getElementById('app-issue-list-root')
+    );
 
 if (document.getElementById('app-issue-create-root'))
-	ReactDOM.render(
-		<IssueCreateEditApp />,
-		document.getElementById('app-issue-create-root')
-	);
+    ReactDOM.render(
+        <IssueCreateEditApp />,
+        document.getElementById('app-issue-create-root')
+    );
 
 if (document.getElementById('app-issue-view-root'))
-	ReactDOM.render(
-		<IssueViewApp />,
-		document.getElementById('app-issue-view-root')
-	);
+    ReactDOM.render(
+        <IssueViewApp />,
+        document.getElementById('app-issue-view-root')
+    );
