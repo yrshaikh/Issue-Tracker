941e9471e90d9ee931017ebf1f4d779959937c7e
Yasser Shaikh <yrshaikh.mail@gmail.com>
2017-10-31 23:38:24 +0700

    fixed more lint errors.

--- Src/WebApplication/wwwroot/clientapp/shared/issue-status.jsx
+++ Src/WebApplication/wwwroot/clientapp/shared/issue-status.jsx
@@ -1,36 +1,57 @@
-import React, { Component } from 'react';
-import PubSub from 'pubsub-js';
-const axios = require('axios');
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class IssueStatus extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			status: props.status
-		};
-    }
-    componentWillReceiveProps(nextProps){
-        this.setState({ status: nextProps.status }); 
-    }
-    getStyle(){
-        var classes = 'badge badge-';
-        if(this.state.status === 'open' || this.state.status === 'reopened')
-            classes += 'success';
-        else if(this.state.status === 'closed')
-            classes += 'danger';
 
-        if(this.props.additionalClasses)
-            classes += ' ' + this.props.additionalClasses;
-        return classes;
+    constructor (props) {
+
+        super(props);
+        this.state = { 'status': props.status };
+
     }
-	render() {
-        var classes = this.getStyle();
-		return (
+
+    componentWillReceiveProps (nextProps) {
+
+        this.setState({ 'status': nextProps.status });
+
+    }
+
+    getStyle () {
+
+        let classes = 'badge badge-';
+        if (this.state.status === 'open' || this.state.status === 'reopened') {
+
+            classes += 'success ';
+
+        } else if (this.state.status === 'closed') {
+
+            classes += 'danger ';
+
+        }
+
+        if (this.props.additionalClasses) {
+
+            classes += this.props.additionalClasses;
+
+        }
+
+        return classes;
+
+    }
+
+    render () {
+
+        const classes = this.getStyle();
+        return (
             <span className={classes}>
                 {this.state.status}
             </span>
-		);
-	}
+        );
+
+    }
+
 }
+
+IssueStatus.PropTypes = {'additionalClasses': PropTypes.string};
 
 export default IssueStatus;
--- Src/WebApplication/wwwroot/clientapp/shared/utils.jsx
+++ Src/WebApplication/wwwroot/clientapp/shared/utils.jsx
@@ -1,16 +1,22 @@
-const getSlug = function(text) {
-    return text.toString().toLowerCase()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-    .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '');            // Trim - from end of text
-};
+const capitalize = function capitalize (input) {
 
-const capitalize = function(input){
-    return input.charAt(0).toUpperCase() + input.slice(1);
-};
+        const one = 1,
+            zero = 0;
 
-export { 
+        return input.charAt(zero).toUpperCase() + input.slice(one);
+
+    },
+    getSlug = function getSlug (text) {
+
+        return text.toString().toLowerCase().
+            replace(/\s+/g, '-').
+            replace(/[^\w\-]+/g, '').
+            replace(/\-\-+/g, '-').
+            replace(/^-+/, '').
+            replace(/-+$/, '');
+
+    };
+
+export {
     getSlug, capitalize
 };
