76e1b55cf09d62e91bd3c687ef0760c8e37645ea
Yasser Shaikh <yrshaikh.mail@gmail.com>
2017-11-18 12:31:23 +0700

    final lint fixs.

--- Src/WebApplication/wwwroot/clientapp/issue-view/components/body/shared/comment-view.jsx
+++ Src/WebApplication/wwwroot/clientapp/issue-view/components/body/shared/comment-view.jsx
@@ -1,53 +1,45 @@
-/* global require */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Gravatar from 'react-gravatar';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 
-const Remarkable = require('remarkable'),
-    md = new Remarkable('full', {
-        'html': true,
-        'linkify': true,
-        'typographer': true
-    });
+const Remarkable = require('remarkable');
+
+const md = new Remarkable('full', {
+    html: true,
+    linkify: true,
+    typographer: true,
+});
 
 class CommentView extends Component {
-
-    constructor (props) {
-
+    constructor(props) {
         super(props);
         this.state = {
-            'createdBy': props.createdBy,
-            'createdByEmail': props.createdByEmail,
-            'createdOn': props.createdOn,
-            'description': props.description,
-            'title': props.title
+            createdBy: props.createdBy,
+            createdByEmail: props.createdByEmail,
+            createdOn: props.createdOn,
+            description: props.description,
+            title: props.title,
         };
-
     }
 
-    static get propTypes () {
-
+    static get propTypes() {
         return {
 
-            'createdBy': PropTypes.string,
-            'createdByEmail': PropTypes.string,
-            'createdOn': PropTypes.string,
-            'description': PropTypes.string,
-            'title': PropTypes.string,
-            'type': PropTypes.string
+            createdBy: PropTypes.string,
+            createdByEmail: PropTypes.string,
+            createdOn: PropTypes.string,
+            description: PropTypes.string,
+            title: PropTypes.string,
+            type: PropTypes.string,
 
         };
-
     }
 
-    render () {
-
+    render() {
         let message = 'commented on';
         if (this.props.type === 'description') {
-
             message = 'opened';
-
         }
         return (
             <div className="form-group comment-box">
@@ -71,28 +63,22 @@ class CommentView extends Component {
                 </div>
             </div>
         );
-
     }
 
-    renderDescription () {
-
+    renderDescription() {
         if (this.state.description) {
-
             const desc = md.render(this.state.description);
             return (
                 <div
-                    dangerouslySetInnerHTML={{'__html': desc}}
+                    dangerouslySetInnerHTML={{ __html: desc }}
                 />
             );
-
         }
 
         return (
             <div className="light-gray">No description provided.</div>
         );
-
     }
-
 }
 
 export default CommentView;
