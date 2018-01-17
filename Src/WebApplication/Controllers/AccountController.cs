70a60b74059549f6d778f1cf504bbe79b37e45ef
Yasser Shaikh <yrshaikh.mail@gmail.com>
2018-01-17 23:34:28 +0700

    Fixed sign in problem.

--- Src/WebApplication/Controllers/AccountController.cs
+++ Src/WebApplication/Controllers/AccountController.cs
@@ -8,57 +8,58 @@ namespace WebApplication.Controllers
 {
     public class AccountController : Controller
     {
-	    private readonly IUserService _userService;
+        private readonly IUserService _userService;
 
-		public AccountController(IUserService userService)
-	    {
-		    _userService = userService;
-	    }
-
-	    public IActionResult SignUp()
+        public AccountController(IUserService userService)
         {
-	        if (User.Identity.IsAuthenticated)
-		        return Redirect("/");
-			return View();
+            _userService = userService;
         }
 
-		[HttpPost]
-	    public async Task<IActionResult> SignUp(SignUpViewModel model)
-	    {
-		    if (ModelState.IsValid)
-		    {
-			    var user = _userService.Create(model);
-			    await new CustomAuthentication().SignIn(HttpContext, user);
-					return Redirect("/");
-			}
-		    return View(model);
-	    }
+        public IActionResult SignUp()
+        {
+            if (User.Identity.IsAuthenticated)
+                return Redirect("/");
+            return View();
+        }
 
-	    public IActionResult SignIn()
-	    {
-		    if (User.Identity.IsAuthenticated)
-				return Redirect("/");
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _userService.Create(model);
+                await new CustomAuthentication().SignIn(HttpContext, user);
+                return Redirect("/");
+            }
+            return View(model);
+        }
 
-			return View();
-	    }
+        public IActionResult SignIn()
+        {
+            if (User.Identity.IsAuthenticated)
+                return Redirect("/");
 
-	    [HttpPost]
-	    public async Task<IActionResult> SignIn(SignInViewModel model)
-	    {
-		    if (ModelState.IsValid
-				&& _userService.Authenticate(model.Email, model.Password))
-		    {
-			    var  user = _userService.Get(model.Email);
-			    await new CustomAuthentication().SignIn(HttpContext, user);
-				return Redirect("/");
-			}
-		    return View(model);
-	    }
+            return View();
+        }
 
-	    public async Task<IActionResult> SignOut()
-	    {
-		    await new CustomAuthentication().SignOut(HttpContext);
-		    return RedirectToAction("signin");
-	    }
-	}
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid
+                && _userService.Authenticate(model.Email, model.Password))
+            {
+                var user = _userService.Get(model.Email);
+                await new CustomAuthentication().SignIn(HttpContext, user);
+                return Redirect("/");
+            }
+            ModelState.AddModelError("Login Failed", "Invalid username or password");
+            return View(model);
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await new CustomAuthentication().SignOut(HttpContext);
+            return RedirectToAction("signin");
+        }
+    }
 }
